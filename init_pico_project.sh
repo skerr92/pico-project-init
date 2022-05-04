affb17d76574d522ac43a37d33d735f01323f1eb
Seth Kerr <41877068+skerr92@users.noreply.github.com>
2022-05-04 06:03:48 -0600

    Create init_pico_project.sh

---
+++ init_pico_project.sh
@@ -0,0 +1,21 @@
+#!/bin/bash +eux
+
+name=$1
+picosdk="../pico-sdk/external/pico_sdk_import.cmake"
+
+mkdir $name
+cd $name
+
+cp $picosdk .
+
+touch CMakeLists.txt
+echo "cmake_minimum_required(VERSION 3.13)
+
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_CXX_STANDARD 17)
+
+# initialize the SDK based on PICO_SDK_PATH
+# note: this must happen before project()
+include(.pico_sdk_import.cmake)
+
+project($name C CXX ASM)
