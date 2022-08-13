5f670dbca64f73dac027d1a50e791706b41ff83e
Seth Kerr <skerr@aggies.ncat.edu>
2022-08-13 12:37:20 -0600

    fix init script

--- init_pico_project.sh
+++ init_pico_project.sh
@@ -1,21 +1,25 @@
 #!/bin/bash +eux
 
+if [ ! -f pico-sdk/pico_sdk_import.cmake ]; then
+  git clone --recursive https://github.com/raspberrypi/pico-sdk.git
+fi
+
 name=$1
 picosdk="../pico-sdk/external/pico_sdk_import.cmake"
 
 mkdir $name
 cd $name
-
+touch $name\.c
 cp $picosdk .
 
 touch CMakeLists.txt
-echo "cmake_minimum_required(VERSION 3.13)
+echo "cmake_minimum_required(VERSION 3.13) 
 
 set(CMAKE_C_STANDARD 11)
 set(CMAKE_CXX_STANDARD 17)
 
-# initialize the SDK based on PICO_SDK_PATH
-# note: this must happen before project()
-include(.pico_sdk_import.cmake)
+# initialize the SDK based on PICO_SDK_PATH 
+# note: this must happen before project() 
+include(.pico_sdk_import.cmake) 
 
-project($name C CXX ASM)
+project($name C CXX ASM) " >> CMakeLists.txt
